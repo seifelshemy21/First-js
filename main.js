a84f7c535eca4188a61ef0f5ca524a03dac084e9
seifelshemy21 <seifelshemy1@gmail.com>
2022-02-08 11:28:32 -0800

    First js

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById ("li1").innerHTML="My Name: Seif El shemy";
+document.getElementById ("li2").innerHTML="Front end developer";
+document.getElementById ("p1").innerHTML="I'm Seif. I'm styding full stack developer. I'm 19 ";
+document.getElementById ("p2").innerHTML="My skils. Html and css ";
+
+
+var numone = 5000/5;
+var numtow = 500*3;
+var num3 = 25+30;
+var num4 = 10*10;
+var num5 = 3*5;
+var num6 = 100/4;
+var num7 = 20*5;
+var num8 = 500+200;
+var num9 = 30/3;
+var num10 = 1000/5;
+var num11 = (numone+numtow+num3+num4+num5+num6+num7+num8+num9+num10)/10
+
+document.getElementById ("num1").innerHTML= numone ;
+document.getElementById ("num2").innerHTML=numtow ;
+document.getElementById ("num3").innerHTML=num3 ;
+document.getElementById ("num4").innerHTML=num4;
+document.getElementById ("num5").innerHTML=num5 ;
+document.getElementById ("num6").innerHTML=num6 ;
+document.getElementById ("num7").innerHTML=num7 ;
+document.getElementById ("num8").innerHTML=num8 ;
+document.getElementById ("num9").innerHTML=num9 ;
+document.getElementById ("num10").innerHTML=num10 ;
+document.getElementById ("num11").innerHTML=num11 ;
